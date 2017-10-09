23f6735a7ca0e8fefca0439d4e155fa9daa9437a
GoldFeniks <ggoldenfeniks@gmail.com>
2017-10-09 10:37:08 +1000

    Change tests

--- PascalCompiler/Tests/Tokenizer/54.pas
+++ PascalCompiler/Tests/Tokenizer/54.pas
@@ -1,3 +1,3 @@
-   bool := false or isGood_Lexxer
-   if isGood_Lexxer and isGood_Program_Code or not isBad_Mood then
-      mark_marks := 10 - day_of_the_day*0
+   bool := false or true
+   if bool and a or not b then
+      result := 10 - y*0

5c365ef650c53377a3683dd564633d0d440cdcf1
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2017-12-12 11:29:06 +1000

    Update test script

--- PascalCompiler/Tests/test.py
+++ PascalCompiler/Tests/test.py
@@ -9,6 +9,7 @@ parser.add_argument('dir', metavar='dir', type=str, help='Tests path');
 parser.add_argument('-co', '--check_output', help='Check exe output', action='store_true')
 parser.add_argument('-go', '--generate_output', help="Don't check anything. Just get output", action='store_true')
 parser.add_argument('argument', metavar='argument', type=str, help='An argument for compiler');
+parser.add_argument('-s', '--suffix', type=str, default=1, help='Suffix to be added to files. Default is empty', const=1, nargs='?')
 args = parser.parse_args()
 
 r = re.compile(r'(?P<name>.+)\.pas')
@@ -16,7 +17,7 @@ f = re.compile(r'(?P<name>[^.]+)$')
 c, w = 0, 0
 devnull = open(os.devnull, 'w')
 
-def run(d, name):
+def run():
     subprocess.call(['ml.exe', '/c', '/coff', 'output.txt'], stdout=devnull, stderr=devnull)
     subprocess.call(['link.exe', '/subsystem:console', 'output.obj'], stdout=devnull, stderr=devnull)
     return subprocess.check_output(['output.exe']).decode('cp1251').replace('\r\n', '\n')
@@ -29,23 +30,23 @@ def test(d):
             subprocess.call(['../../x64/Debug/PascalCompiler.exe', '-' + args.argument, d + '/' + i, 'output.txt']) 
             a, b = '', ''
             if args.check_output:
-                b = run(d, name.group('name'))
+                b = run()
                 if args.generate_output:
-                    print(b, file=open(d + '/{}.output'.format(name.group('name')), 'w'), end='')
+                    print(b, file=open(d + '/{}.output'.format(name.group('name') + args.suffix), 'w'), end='')
                     continue   
-                a = open(d + '/{}.output'.format(name.group('name'))).read()
+                a = open(d + '/{}.output'.format(name.group('name') + args.suffix)).read()
             else:
                 b = open('output.txt').read()
                 if args.generate_output:
-                    print(b, file=open(d + '/{}.result'.format(name.group('name')), 'w'), end='')
+                    print(b, file=open(d + '/{}.result'.format(name.group('name') + args.suffix), 'w'), end='')
                     continue
-                a = open(d + '/{}.result'.format(name.group('name'))).read()
+                a = open(d + '/{}.result'.format(name.group('name') + args.suffix)).read()
             if a != b:
                 print('Test {}/{} failed'.format(d, i))
                 if args.check_output:
-                    print(b, file=open(d + '/{}.output'.format(name.group('name')), 'w'), end='')
+                    print(b, file=open(d + '/{}.output'.format(name.group('name') + args.suffix), 'w'), end='')
                 else:
-                    print(b, file=open(d + '/{}.result'.format(name.group('name')), 'w'), end='')
+                    print(b, file=open(d + '/{}.result'.format(name.group('name') + args.suffix), 'w'), end='')
                 w += 1
             else:
                 print('Test {}/{} passed'.format(d, i))
